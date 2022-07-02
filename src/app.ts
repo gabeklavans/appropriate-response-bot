10a27307e90bc4ca447f428b5f183a7c34fe9816
Gabriel Klavans <gabeklav@fastmail.com>
2022-07-02 11:16:23 -0400

    Switch to using grammy

--- src/app.ts
+++ src/app.ts
@@ -1,19 +1,12 @@
-/* Uncomment this if you wanna use types smh */
-import { Telegraf } from "telegraf";
-// const Telegraf = require("telegraf");
 import * as dotenv from "dotenv";
-import { Message, Update } from "typegram";
 dotenv.config();
+import { Bot } from "grammy";
 
 if (process.env.BOT_TOKEN === undefined) {
   throw new TypeError("BOT_TOKEN must be provided!");
 }
 
-const bot = new Telegraf(process.env.BOT_TOKEN);
-
-bot.telegram.getMe().then((botInfo) => {
-  (bot as any).options.username = botInfo.username;
-});
+const bot = new Bot(process.env.BOT_TOKEN);
 
 const RESPONSE_CHANCE = 0.01;
 const RESPONSES = [
@@ -36,26 +29,30 @@ const RESPONSES = [
   "OK, I failed at that.",
   "immigrants,, they get the job done",
   "you must not be talking about *women*",
-  "OR SHE"
+  "OR SHE",
 ];
 
-bot.start((ctx) => {
-  ctx.reply("You'll be hearing from me.")
+bot.command("start", (ctx) => {
+  ctx.reply("You'll be hearing from me.");
 });
 
 bot.on("message", (ctx) => {
-  const isMention = (ctx.message as any).entities ? (ctx.message as any).entities[0].type === 'mention' : false;
+  const isMention = (ctx.message as any).entities
+    ? (ctx.message as any).entities[0].type === "mention"
+    : false;
 
   if (isMention) {
     // got mentioned, figure out whether to reply to sender or the sender's replied message
-    const repliedMsgId = (ctx.message as Update.New & Update.NonChannel & Message.TextMessage).reply_to_message?.message_id;
+    const repliedMsgId = ctx.message.reply_to_message?.message_id;
     const response = RESPONSES[Math.floor(Math.random() * RESPONSES.length)];
     if (repliedMsgId) {
       // reply to reply
-      ctx.replyWithMarkdown(response, { reply_to_message_id: repliedMsgId });
+      ctx.reply(response, { reply_to_message_id: repliedMsgId });
     } else {
       // reply to sender
-      ctx.replyWithMarkdown(response, { reply_to_message_id: ctx.message.message_id })
+      ctx.reply(response, {
+        reply_to_message_id: ctx.message.message_id,
+      });
     }
   } else {
     // just a normal message, decide to randomly respond
@@ -66,13 +63,8 @@ bot.on("message", (ctx) => {
       }, 420);
     }
   }
-
 });
 
-bot.launch().then(() => {
+bot.start().then(() => {
   console.log("Started bot");
 });
-
-// Enable graceful stop
-process.once("SIGINT", () => bot.stop("SIGINT"));
-process.once("SIGTERM", () => bot.stop("SIGTERM"));
